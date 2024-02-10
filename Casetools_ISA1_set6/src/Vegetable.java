9e94daa5186c85fc10bde6731e91216d16e968f7
nishimay <nishiparodkar@gmail.com>
2024-02-10 10:13:29 +0530

    com1

---
+++ Casetools_ISA1_set6/src/Vegetable.java
@@ -0,0 +1,17 @@
+
+public class Vegetable {
+
+	String name;
+	Vegetable(String name){
+		this.name=name;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Vegetable spinach= new Vegetable("spinach");
+		System.out.println("Vegetable is"+spinach.name);
+	}
+
+}
